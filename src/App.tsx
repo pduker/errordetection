e668ad1788082aa2c8eacd0438d5e401fb6957bc
weldin <weldindunn20@gmail.com>
2023-10-22 14:18:32 -0400

    created really basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import logo from './assets/doof.png';
 import './App.css';
 
@@ -7,9 +11,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Error Detectinator!</p>
-        <Navbar/>
+        <Navbar className="Home-bar" fixed='top'>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="75"
+              height="75"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Brand>Error Detectinator!</Navbar.Brand>
+          <Nav className='Home-nav' justify>
+            <Nav.Link href="#exercises">Exercises</Nav.Link>
+            <Nav.Link href="#exercise-management">Exercise Management</Nav.Link>
+            <Nav.Link href="#help">Help</Nav.Link>
+          </Nav>
+        </Navbar>
       </header>
     </div>
   );
