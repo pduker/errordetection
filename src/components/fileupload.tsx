1d9dc92034c30de57a454ddc61fc2bfbbcf837c4
cstet23 <cstetler@udel.edu>
2023-11-30 15:25:21 -0500

    Trying to get xml2abc converter working, slight progress but not functional yet

--- src/components/fileupload.tsx
+++ src/components/fileupload.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import $ from 'jquery';
+//@ts-ignore
+import { vertaal } from 'xml2abc';
 
 export default function FileUpload (): JSX.Element{
-
+    <script src="xml2abc.js"></script>
     //state declarations, can be used to access file in the future
     const [file, setFile] = useState<File>();
     const [msgContent, setMsgContent] = useState<string>("Nothing selected.");
@@ -17,6 +20,40 @@ export default function FileUpload (): JSX.Element{
         //checks for .musicxml and .mp3 files, otherwise returns error msg (can be easily changed)
         if (files[0].name.endsWith(".musicxml")) {
             setMsgContent("xml file selected.");
+            var xmldata: XMLDocument;
+            const fileReader = new FileReader();
+            fileReader.onload = () => {
+                const fileContent = fileReader.result as string;
+                console.log("file is now string");
+                xmldata = $.parseXML(fileContent);
+                console.log("file is parsed");
+            }
+            fileReader.onloadend = () => {
+                var options = { u:0, b:0, n:0,
+                c:0, v:0, d:0,  
+                m:0, x:0, t:0,  
+                v1:0, noped:0,  
+                stm:0,          
+                p:'f', s:0 };
+                var result = vertaal(xmldata, options);
+                console.log("vertaal called");
+                var abcText = result[0];
+                console.log("file? " + abcText);
+            };
+            console.log("now reading");
+            fileReader.readAsText(files[0]);
+
+            
+            // var options = { u:0, b:0, n:0,
+            // c:0, v:0, d:0,  
+            // m:0, x:0, t:0,  
+            // v1:0, noped:0,  
+            // stm:0,          
+            // p:'f', s:0 };
+            // var result = vertaal(xmldata, options);
+            // console.log("vertaal called");
+            // var abcText = result[0];
+            // console.log("file? " + abcText);
         } else if (files[0].name.endsWith(".mp3")) {
             setMsgContent("mp3 file selected.");
         } else {
