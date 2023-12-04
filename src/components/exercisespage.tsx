3156629c69ecb898421c4987b234102c776111cc
cstet23 <cstetler@udel.edu>
2023-12-03 21:01:55 -0500

    xml to abc conversion working! :) next up is making an exercise component

--- src/components/exercisespage.tsx
+++ src/components/exercisespage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import  abcjs from 'abcjs';
+import { FileUpload } from './fileupload';
 
 export function ExercisesPage({ 
     correctAnswers, 
@@ -11,18 +12,23 @@ export function ExercisesPage({
     const [output, setOutput] = useState<String>();
     const [selectedAnswers, setSelectedAnswers] = useState<Array<String>>([]);
     const [itemList, setItemList] = useState<JSX.Element[]>();
+    const [abcFile, setAbcFile] = useState<string>();
+    const [ana, setAna] = useState<string>();
 
     const clickListener = function (abclem:object, tuneNumber: number,classes:string, analysis:abcjs.ClickListenerAnalysis, drag:abcjs.ClickListenerDrag){
     
         var op = "abcelem: [Object]<br>tuneNumber: " + tuneNumber + "<br>classes: " + classes + "<br>analysis: " + JSON.stringify(analysis) + "<br>drag: " + JSON.stringify(drag);
         setOutput(op);
+        setAna(JSON.stringify(analysis));
         var test = document.querySelector(".clicked-info");
-        if(test !== null) {test.innerHTML = "<div class='label'>Clicked info:</div>" + op;}}
+        if(test !== null) {test.innerHTML = "<div class='label'>Clicked info:</div>" + op;}
+    }
     
     const loadScore = function() {
-        var abcString = "X:1\nZ:Copyright ©\n%%scale 0.83\n%%pagewidth 21.59cm\n%%leftmargin 1.49cm\n%%rightmargin 1.49cm\n%%score [ 1 2 ] 3\nL:1/4\nQ:1/4=60\nM:4/4\nI:linebreak $\nK:Amin\nV:1 treble nm=Flute snm=Fl.\nV:2 treble transpose=-9 nm=Alto Saxophone snm=Alto Sax.\nV:3 bass nm=Violoncello snm= Vc.\nV:1\nc2 G3/2 _B/ | _A/_B/ c _d f | _e _d c2 |] %3\nV:2\n[K:F#min] =c d c3/2 e/ | =f f/e/ d2 | =f e f2 |] %3\nV:3\n_A,,2 _E,,2 | F,,2 _D,,2 | _E,,2 _A,,2 |] %3";
+        // sample file: "X:1\nZ:Copyright ©\n%%scale 0.83\n%%pagewidth 21.59cm\n%%leftmargin 1.49cm\n%%rightmargin 1.49cm\n%%score [ 1 2 ] 3\nL:1/4\nQ:1/4=60\nM:4/4\nI:linebreak $\nK:Amin\nV:1 treble nm=Flute snm=Fl.\nV:2 treble transpose=-9 nm=Alto Saxophone snm=Alto Sax.\nV:3 bass nm=Violoncello snm= Vc.\nV:1\nc2 G3/2 _B/ | _A/_B/ c _d f | _e _d c2 |] %3\nV:2\n[K:F#min] =c d c3/2 e/ | =f f/e/ d2 | =f e f2 |] %3\nV:3\n_A,,2 _E,,2 | F,,2 _D,,2 | _E,,2 _A,,2 |] %3"
+        var abcString = abcFile;
         var el = document.getElementById("target");
-        if(el != null){abcjs.renderAbc(el,abcString,{ clickListener: clickListener});}
+        if(el !== null && abcString !== undefined){abcjs.renderAbc(el,abcString,{ clickListener: clickListener, selectTypes: ["note"]});}
     }
 
     const selectAnswer = function() {
@@ -40,9 +46,11 @@ export function ExercisesPage({
             <h2>Welcome to the Exercises Page!</h2>
 
             <span>
+                <FileUpload setAbcFile={setAbcFile}></FileUpload>
                 <button onClick={loadScore}>Load Score</button>
                 <div id ="target"></div>
                 <div className="clicked-info"></div>
+                <div>Analysis: {ana}</div>
                 <button onClick={selectAnswer}>Select Answer</button>
                 <div>Currently selected answers:</div>
                 <ul>{itemList}</ul>
--- src/components/fileupload.tsx
+++ src/components/fileupload.tsx
@@ -1,12 +1,36 @@
 import { useState } from 'react';
-import $ from 'jquery';
-//@ts-ignore
 import { vertaal } from 'xml2abc';
 
-export default function FileUpload (): JSX.Element{
+
+export function FileUpload ({
+    setAbcFile
+}: {
+    setAbcFile: ((newFile: string) => void) | null;
+}): JSX.Element{
     //state declarations, can be used to access file in the future
     const [file, setFile] = useState<File>();
     const [msgContent, setMsgContent] = useState<string>("Nothing selected.");
+
+    const abcTranslate = function (fileContent: string) {
+        var domparser = new DOMParser();
+        var xmldata = domparser.parseFromString(fileContent, 'application/xml');
+        console.log("file is parsed");
+        var options = { u:0, b:0, n:0,
+        c:0, v:0, d:0,  
+        m:0, x:0, t:0,  
+        v1:0, noped:0,  
+        stm:0,          
+        p:'f', s:0 };
+        var result = vertaal(xmldata, options);
+        console.log("vertaal called");
+        var abcText = result[0];
+        var errorText = result[1];
+        if (setAbcFile != null) {
+            setAbcFile(abcText);
+            console.log("file? " + abcText + "<- should be here");
+            console.log("error text: " + errorText);
+        }
+    }
 
     //function to update state whenever uploaded file is changed
     const fileChange = function (e: React.ChangeEvent<HTMLInputElement>) {
@@ -19,26 +43,12 @@ export default function FileUpload (): JSX.Element{
         //checks for .musicxml and .mp3 files, otherwise returns error msg (can be easily changed)
         if (files[0].name.endsWith(".musicxml")) {
             setMsgContent("xml file selected.");
-            var xmldata: XMLDocument;
             const fileReader = new FileReader();
             fileReader.onload = () => {
                 const fileContent = fileReader.result as string;
                 console.log("file is now string");
-                xmldata = $.parseXML(fileContent);
-                console.log("file is parsed");
+                abcTranslate(fileContent);
             }
-            fileReader.onloadend = () => {
-                var options = { u:0, b:0, n:0,
-                c:0, v:0, d:0,  
-                m:0, x:0, t:0,  
-                v1:0, noped:0,  
-                stm:0,          
-                p:'f', s:0 };
-                var result = vertaal(xmldata, options);
-                console.log("vertaal called");
-                var abcText = result[0];
-                console.log("file? " + abcText);
-            };
             console.log("now reading");
             fileReader.readAsText(files[0]);
 
