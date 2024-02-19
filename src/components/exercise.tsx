d312b580594406216e3279dabab404cc833ca462
cstet23 <cstetler@udel.edu>
2024-02-19 16:19:40 -0500

    Multiple note selection (no Christmas lights)

--- src/components/exercise.tsx
+++ src/components/exercise.tsx
@@ -19,7 +19,7 @@ export function Exercise({
     setFiles:((newFile: File[]) => void);
 }) {
     var abc,ans,feed, color: string;
-    var visualObjs: any[], visualObj;
+    var selNotes: any[] = [];
     abc = "";
     ans = "";
     feed = "";
@@ -34,11 +34,7 @@ export function Exercise({
     const [itemList, setItemList] = useState<JSX.Element[]>();
     
     const [abcFile, setAbcFile] = useState<string>(abc);
-    const [ana, setAna] = useState<string>();
-
-    /* window.onload = function() {
-        let abc_editor = new abcjs.Editor("abc", {canvas_id: "paper"});
-    } */   
+    const [ana, setAna] = useState<string>(); 
 
     const setClass = function (elemset: any, addClass: any, removeClass: any, color: any) {
         if (!elemset)
@@ -60,26 +56,26 @@ export function Exercise({
         }
     };
 
-    const highlight = function (note: any, klass: any) {
+    const highlight = function (note: any, klass: any, clicked: boolean) {
         
         var selTim = note.abselem.elemset[0].getAttribute("selectedTimes");
+        if (clicked) selTim++;
+        if (selTim >= 4) selTim = 0;
         if (klass === undefined)
             klass = "abcjs-note_selected";
         if (selTim <= 0) {
-            color = "#ff0000";
+            color = "#000000";
         }
         if (selTim == 1) {
-            color = "#00ff00";
+            color = "#ff0000";
         }
         if (selTim == 2) {
-            color = "#0000ff";
+            color = "#00ff00";
         }
         if (selTim == 3) {
-            color = "#000000";
-            selTim = -1;
+            color = "#0000ff";
         }
-        selTim++;
-        note.abselem.elemset[0].setAttribute("selectedTimes", selTim);
+        if (clicked) note.abselem.elemset[0].setAttribute("selectedTimes", selTim);
         setClass(note.abselem.elemset, klass, "", color);
     };
 
@@ -88,14 +84,20 @@ export function Exercise({
         var op = JSON.stringify(drag.index);
         setOutput(op);
         setAna(JSON.stringify(drag.index));
-        visualObj = visualObjs[0];
         var note = abcelem;
         if(!(note.abselem.elemset[0].getAttribute("selectedTimes"))) {
             note.abselem.elemset[0].setAttribute("selectedTimes", 0)
         }
-        console.log(note);
-        console.log(note.abselem.elemset[0].getAttribute("selectedTimes"));
-        highlight(note, undefined);
+        if(!selNotes.includes(note)) {
+            selNotes[selNotes.length] = note;
+        }
+        for (var i=0; i<selNotes.length; i++) {
+            if(selNotes[i] === note) {
+                highlight(selNotes[i], undefined, true);
+            } else {
+                highlight(selNotes[i], undefined, false);
+            }
+        }
         var test = document.querySelector(".clicked-info");
         if(test !== null) {test.innerHTML = "<div class='label'>Clicked info:</div>" + op;}
     }
@@ -104,14 +106,13 @@ export function Exercise({
         // sample file: "X:1\nZ:Copyright ©\n%%scale 0.83\n%%pagewidth 21.59cm\n%%leftmargin 1.49cm\n%%rightmargin 1.49cm\n%%score [ 1 2 ] 3\nL:1/4\nQ:1/4=60\nM:4/4\nI:linebreak $\nK:Amin\nV:1 treble nm=Flute snm=Fl.\nV:2 treble transpose=-9 nm=Alto Saxophone snm=Alto Sax.\nV:3 bass nm=Violoncello snm= Vc.\nV:1\nc2 G3/2 _B/ | _A/_B/ c _d f | _e _d c2 |] %3\nV:2\n[K:F#min] =c d c3/2 e/ | =f f/e/ d2 | =f e f2 |] %3\nV:3\n_A,,2 _E,,2 | F,,2 _D,,2 | _E,,2 _A,,2 |] %3"
         var abcString = abcFile;
         var el = document.getElementById("target");
-        if(el !== null && abcString !== undefined){visualObjs = abcjs.renderAbc(el,abcString,{ clickListener: clickListener, selectTypes: ["note"]});}
+        if(el !== null && abcString !== undefined){abcjs.renderAbc(el,abcString,{ clickListener: clickListener, selectTypes: ["note"]});}
         /* var el2 = document.getElementById("test");
         var abc = document.getElementById("text");
         if(el2 !== null && abc !== null) {
             //let editor = new abcjs.Editor("abc", {canvas_id: "el"});
             abc.innerHTML = abcFile;
         } */
-        console.log(visualObjs);
         
     }
     const save = function(){
