84cdebb20659550534fba2dadbf3a79700a40157
sydni wright <sydniw@udel.edu>
2025-02-25 19:22:48 -0500

    added few comments in audio handler

--- src/components/audiohandler.tsx
+++ src/components/audiohandler.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 
-
+//file for handling the audio files and turning them into JSX elements that we can use on the screen
 export default function AudioHandler({ file }: { file: File }): JSX.Element {
   //state declarations, can be used to access file in the future
   const [msgContent] = useState<string>("");
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  //sets the audio that is uploaded
+  //sets the audio file that is uploaded
   useEffect(() => {
     if (file.name !== undefined) 
     if (file.name.endsWith(".mp3")) {
       
+      //creates an object url given the file, alows us to actually use the audio content
       const objectURL = URL.createObjectURL(file);
       if (audioRef.current) {
         audioRef.current.src = objectURL;
