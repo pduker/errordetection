d71fd2adc8515488c1ad9bb22c036548a0423c21
Jared Miller <jaredmiller219@gmail.com>
2025-10-06 15:57:05 -0400

    updated help section explanation spacing

--- src/components/helpSections/CheckAnswers.tsx
+++ src/components/helpSections/CheckAnswers.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import check from "../../assets/check-answer.png";
 import { Link } from "react-router-dom";
 
 export default function CheckAnswers() {
     return (
-        <div style={{padding: 20}}>
+        <div style={{padding: 20, display: "flex", flexDirection: "column", alignItems: "stretch", width: "100%", minHeight: "100%", boxSizing: "border-box", flex: 1}}>
             <div style={{marginBottom: 12}}>
                 <Link to="/help">← Back to Help</Link>
             </div>
              <h2>Check Answers</h2>
-             <p>After selecting all errors, click "Check Answers" to receive feedback. There will be error/highlight hints and written feedback for incorrect selections.</p>
-             <img alt="check answer" src={check} width="700" style={{maxWidth: "100%"}} />
+             <p style={{ textAlign: "left" }}>After selecting all errors, click "Check Answers" to receive feedback. There will be error/highlight hints and written feedback for incorrect selections.</p>
+             <img alt="check answer" src={check} style={{width: "100%", maxWidth: "100%", height: "auto", display: "block"}} />
          </div>
      );
  }
--- src/components/helpSections/NoteKey.tsx
+++ src/components/helpSections/NoteKey.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import noteKey from "../../assets/note-color-key.png";
 import { Link } from "react-router-dom";
 
 export default function NoteKey() {
     return (
-        <div style={{padding: 20}}>
+        <div style={{padding: 20, display: "flex", flexDirection: "column", alignItems: "stretch", width: "100%", minHeight: "100%", boxSizing: "border-box", flex: 1}}>
             <div style={{marginBottom: 12}}>
                 <Link to="/help">← Back to Help</Link>
             </div>
              <h2>Color Key</h2>
-             <p>The key shows which color corresponds to each type of error.</p>
-             <img alt="note-color-key" src={noteKey} width="500" style={{maxWidth: "100%"}} />
+             <p style={{ textAlign: "left" }}>The key shows which color corresponds to each type of error.</p>
+             <img alt="note-color-key" src={noteKey} style={{width: "100%", maxWidth: "100%", height: "auto", display: "block"}} />
          </div>
      );
  }
