6fa9eb32f49d47f3dfe01f8ac8655cc51c37eff7
Jared Miller <jaredmiller219@gmail.com>
2025-10-08 10:22:28 -0400

    put names in about page into a list

--- src/components/aboutpage.tsx
+++ src/components/aboutpage.tsx
@@ -14,8 +14,24 @@ export function AboutPage() {
                     This website is an error detection practice tool! Choose a category of exercises above to get started, or visit the Help page to learn more. See a problem? Please get in touch with Phil Duker.
                 </div>
                 <br />
-                <div style={{ textAlign: 'left' }}>
-                    This project was led by Phil Duker with the help of the following wonderful UD students: Alex Daley, Weldin Dunn, Benjamin McMonagle, Sean O’Sullivan, Sonika Sharma, Colin Stetler, Victor Umoren-Udo, Lillian Woulfe, Adam Beck, Sehee Hwang, Michael Murphy, Mann Patel, Tyran Rice, and Sydni Wright.
+<div style={{ textAlign: 'left' }}>
+                    This project was led by Phil Duker with the help of the following wonderful UD students:
+                    <ul>
+                        <li>Alex Daley</li>
+                        <li>Weldin Dunn</li>
+                        <li>Benjamin McMonagle</li>
+                        <li>Sean O'Sullivan</li>
+                        <li>Sonika Sharma</li>
+                        <li>Colin Stetler</li>
+                        <li>Victor Umoren-Udo</li>
+                        <li>Lillian Woulfe</li>
+                        <li>Adam Beck</li>
+                        <li>Sehee Hwang</li>
+                        <li>Michael Murphy</li>
+                        <li>Mann Patel</li>
+                        <li>Tyran Rice</li>
+                        <li>Sydni Wright</li>
+                    </ul>
                 </div>
                 <br />
                 <div style={{ textAlign: 'left' }}>
