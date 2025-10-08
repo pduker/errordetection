66bd4f927ed7e73b51546b873ac4ce5d87ac8fdb
Jared Miller <jaredmiller219@gmail.com>
2025-10-08 10:32:09 -0400

    updated image spacing

--- src/components/helpSections/CheckAnswers.tsx
+++ src/components/helpSections/CheckAnswers.tsx
@@ -7,9 +7,19 @@ export default function CheckAnswers() {
             <div style={{marginBottom: 12}}>
                 <Link to="/help">← Back to Help</Link>
             </div>
-             <h2>Check Answers</h2>
-             <p style={{ textAlign: "left" }}>After selecting all errors, click "Check Answers" to receive feedback. There will be error/highlight hints and written feedback for incorrect selections.</p>
-             <img alt="check answer" src={check} style={{width: "100%", maxWidth: "100%", height: "auto", display: "block"}} />
+            <h2>Check Answers</h2>
+            <p style={{ textAlign: "left" }}>After selecting all errors, click "Check Answers" to receive feedback. There will be error/highlight hints and written feedback for incorrect selections.</p>
+            <img
+                alt="note click"
+                src={check}
+                style={{
+                    width: "100%",
+                    maxWidth: "100%",
+                    height: "auto",
+                    display: "block",
+                    transform: "translateY(-20px)"
+                }}
+            />
          </div>
      );
  }
