72c0f6aeaf1448e88bc84fbbff20d2316d230d1c
Jared Miller <jaredmiller219@gmail.com>
2025-10-27 10:12:41 -0400

    working on sidebar css, (ignore the sidebar testing css file, it is only for making sure it works

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -11,8 +11,17 @@ interface AppSidebarProps {
 // 2. Update the component to accept and use the props
 export const AppSidebar: React.FC<AppSidebarProps> = ({ isCollapsed }) => {
   return (
-    // 3. Pass the 'isCollapsed' prop to the Sidebar component
-    <Sidebar collapsed={isCollapsed}>
+    <Sidebar 
+      collapsed={isCollapsed}
+      // --- ADD THESE STYLES ---
+      rootStyles={{
+        height: '100vh',    // Set the height to 100% of the viewport
+        position: 'sticky', // Make it stick to the top
+        top: 0,             // Anchor it to the top
+        width: '250px',
+      }}
+      // -----------------------
+    >
       <Menu
         menuItemStyles={{
           button: {
@@ -24,16 +33,27 @@ export const AppSidebar: React.FC<AppSidebarProps> = ({ isCollapsed }) => {
           },
         }}
       >
-        <MenuItem component={<NavLink to="/documentation" />}> Documentation</MenuItem>
+        {/* <MenuItem component={<NavLink to="/documentation" />}> Documentation</MenuItem>
         <MenuItem component={<NavLink to="/calendar" />}> Calendar</MenuItem>
-        <MenuItem component={<NavLink to="/e-commerce" />}> E-commerce</MenuItem>
+        <MenuItem component={<NavLink to="/e-commerce" />}> E-commerce</MenuItem> */}
         
-        {/* Your SubMenu from before */}
-        <SubMenu label="Charts">
+        {/* <SubMenu label="Charts">
           <MenuItem component={<NavLink to="/charts/pie" />}> Pie charts </MenuItem>
           <MenuItem component={<NavLink to="/charts/line" />}> Line charts </MenuItem>
           <MenuItem component={<NavLink to="/charts/bar" />}> Bar charts </MenuItem>
+        </SubMenu> */}
+
+        {/* --- I've added extra items below to demonstrate the scrolling --- */}
+        <MenuItem> Tags </MenuItem>
+        <MenuItem> Pitch </MenuItem>
+        <MenuItem> Intonation </MenuItem>
+        <MenuItem> Rhythm </MenuItem>
+        <SubMenu label="Advanced">
+          <MenuItem> Text </MenuItem>
+          <MenuItem> Text </MenuItem>
+          <MenuItem> Text </MenuItem>
         </SubMenu>
+        {/* --------------------------------------------------------------- */}
       </Menu>
     </Sidebar>
   );
