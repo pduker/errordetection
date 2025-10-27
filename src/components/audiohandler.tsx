7c7166fa448cf6d2e4044f2ab1c43c244ddb54b3
Jared Miller <jaredmiller219@gmail.com>
2025-10-27 08:05:46 -0400

    working on fixing errors

--- src/components/audiohandler.tsx
+++ src/components/audiohandler.tsx
@@ -1,26 +1,7 @@
+import { getBlob } from 'firebase/storage';
 import { useEffect, useRef, useState } from 'react';
 
 //file for handling the audio files and turning them into JSX elements that we can use on the screen
-<<<<<<< HEAD
-export default function AudioHandler({ file }: { file: File }): JSX.Element {
-  //state declarations, can be used to access file in the future
-  const [msgContent] = useState<string>("");
-  const audioRef = useRef<HTMLAudioElement>(null);
-
-  //sets the audio file that is uploaded
-  useEffect(() => {
-    if (file.name !== undefined) 
-    if (file.name.endsWith(".mp3")) {
-      
-      //creates an object url given the file, alows us to actually use the audio content
-      const objectURL = URL.createObjectURL(file);
-      if (audioRef.current) {
-        audioRef.current.src = objectURL;
-      }
-      return () => {
-        URL.revokeObjectURL(objectURL);
-      };
-=======
 export default function AudioHandler({ file }: { file: string | File }): JSX.Element {
   const [msgContent] = useState<string>("");
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -30,7 +11,7 @@ export default function AudioHandler({ file }: { file: string | File }): JSX.Ele
 
     if(typeof file === "string") { // file is a URL to a file
       // get the audio data as a binary blob
-      getBlob(storageRef(storage, file)).then(blob => {
+      getBlob(StorageRef(Storage, file)).then((blob: BlobPart) => {
         const fileObject = new File([blob], file, {type: "audio/mpeg"}); // make a file out of the binary data
         const objectURL = URL.createObjectURL(fileObject); // create a URL to link it to an audio element
         if(audioRef.current) audioRef.current.src = objectURL; // set audio element's URL to the URL we just made
@@ -39,7 +20,6 @@ export default function AudioHandler({ file }: { file: string | File }): JSX.Ele
       // don't do any of that fancy new File object stuff
       const objectURL = URL.createObjectURL(file);
       if(audioRef.current) audioRef.current.src = objectURL;
->>>>>>> a417ae6393129ccf8adfa1ecdf07818dd730fa72
     }
   }, [file]);
 
