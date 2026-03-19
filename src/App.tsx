15723bdac31929edc3f893fec6cc543698a4d346
BOGUS NAME <siromero@udel.edu>
2026-03-19 11:33:08 -0400

    Changed nav bar posititioning a bit

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function Header({ authorized, resetScrollPosition }: { authorized: boolean; rese
       {/* Centered title */}
       <div style={{ position: 'absolute', right: '50%', transform: 'translateX(50%)', textAlign: 'center', pointerEvents: "none" }}>
         <div style={{display: 'flex', flexDirection:'column', alignItems: 'center', gap:0}}>
-          <Navbar.Brand className='Home-title' style={{ color: '#114b96', display: 'block', marginTop: '6px', lineHeight:1, fontSize: "clamp(1.2rem, 3vw, 2rem)" }}>
+          <Navbar.Brand className='Home-title' style={{ color: '#114b96', display: 'block', marginTop: '12px', lineHeight:1, fontSize: "clamp(1.2rem, 3vw, 2rem)" }}>
           University of Delaware
           </Navbar.Brand>
           <Navbar.Brand className='Home-title' style={{ color: '#114b96', display: 'block', fontSize: "clamp(0.8rem, 2vw, 1.2rem)" }}>
